b5c98ebafc8e25b0bf9c9d2506ea734d8dd81931
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-29 14:44:22 -0400

    package binary

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,19 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q epsxe | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(cat ~/version)
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://aur.archlinux.org/cgit/aur.git/plain/epsxe.png?h=epsxe
+export DESKTOP=https://aur.archlinux.org/cgit/aur.git/plain/epsxe.desktop\?h\=epsxe
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
-export PATH_MAPPING='/opt/epsxe:${SHARUN_DIR}'
 
 # Deploy dependencies
-quick-sharun /opt/epsxe/*
+quick-sharun ./epsxe_x64
 
 # Additional changes can be done in between here
 
