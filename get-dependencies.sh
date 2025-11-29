fc202d129045c8f9c3472a2342eeb295550471db
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-29 14:49:39 -0400

    rename bin

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,7 +14,7 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 make-aur-package --chaotic-aur libcurl-compat
-make-aur-package --chaotic-aur ncurses5-compat-libs 
+make-aur-package --chaotic-aur ncurses5-compat-libs
 
 echo "Getting epsxe binary..."
 echo "---------------------------------------------------------------"
@@ -23,9 +23,10 @@ BINARY=$(wget --retry-connrefused --tries=30 https://www.epsxe.com/download.php 
 wget --retry-connrefused --tries=30 "$BINARY"
 unzip ./*.zip
 chmod +x ./epsxe_x64
+mv -v ./epsxe_x64 ./epsxe
 
 # this app needs an older version of libcurl to work
-patchelf --replace-needed libcurl.so.4 libcurl-compat.so.4.8.0 ./epsxe_x64
+patchelf --replace-needed libcurl.so.4 libcurl-compat.so.4.8.0 ./epsxe
 
-./epsxe_x64 -v | awk 'END {print $NF}' > ~/version
+./epsxe -v | awk 'END {print $NF}' > ~/version
 
--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun ./epsxe_x64
+quick-sharun ./epsxe
 
 # Additional changes can be done in between here
 
