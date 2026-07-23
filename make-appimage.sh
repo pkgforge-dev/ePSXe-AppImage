a7e6e3e49e8a0151de2823879f87ca6f55d64a4c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-07-23 12:37:13 -0400

    test in prod

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,6 +11,9 @@ export ICON=https://aur.archlinux.org/cgit/aur.git/plain/epsxe.png?h=epsxe
 export DESKTOP=https://aur.archlinux.org/cgit/aur.git/plain/epsxe.desktop\?h\=epsxe
 export DEPLOY_OPENGL=1
 
+wget https://raw.githubusercontent.com/Samueru-sama/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh -O /usr/local/bin/quick-sharun
+chmod +x /usr/local/bin/quick-sharun
+
 # Deploy dependencies
 quick-sharun \
 	./epsxe             \
