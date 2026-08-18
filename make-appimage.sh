056e2e05f2cdb017e55e6ef7d592af958eef181d
Link4Electronics <link4electronics@gmail.com>
2026-08-18 13:36:34 -0300

    update template and hook (#10)

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://aur.archlinux.org/cgit/aur.git/plain/epsxe.png?h=epsxe
 export DESKTOP=https://aur.archlinux.org/cgit/aur.git/plain/epsxe.desktop\?h\=epsxe
